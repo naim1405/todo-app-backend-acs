624c20911475a046789eb6c743b61d6124ee7e8e
naim <naim.baijid@gmail.com>
2025-03-24 10:46:33 +0600

    schema update

--- prisma/migrations/20250323174329_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Refresh_Token" (
-    "id" TEXT NOT NULL,
-    "token" TEXT NOT NULL,
-    "isActive" BOOLEAN NOT NULL,
-
-    CONSTRAINT "Refresh_Token_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20250324044154_/migration.sql
+++ prisma/migrations/20250324044154_/migration.sql
@@ -1,4 +1,13 @@
 -- CreateTable
+CREATE TABLE "Refresh_Token" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Refresh_Token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -12,7 +21,8 @@ CREATE TABLE "User" (
 CREATE TABLE "todo" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "detail" TEXT NOT NULL,
+    "details" TEXT NOT NULL,
+    "isDone" BOOLEAN NOT NULL,
     "userId" TEXT NOT NULL,
 
     CONSTRAINT "todo_pkey" PRIMARY KEY ("id")
